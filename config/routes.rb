b3abb1b35da381485f3e5d444872ea501eb9279f
Reiner Marquez <rmaceissoft@gmail.com>
2012-03-15 14:19:17 -0500

    adding home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Timetracker::Application.routes.draw do
+
   devise_for :users
 
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
@@ -53,7 +54,7 @@ Timetracker::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
