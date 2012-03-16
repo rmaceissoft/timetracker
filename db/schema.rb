153f1777dbf3ccb46c5cc7aeaf8677d6ce949f50
Reiner Marquez <rmaceissoft@gmail.com>
2012-03-16 16:04:51 -0500

    enabled registration with confirmation, added another accesible fields list for admin role

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120316154543) do
+ActiveRecord::Schema.define(:version => 20120316164607) do
 
   create_table "invoice_cycles", :force => true do |t|
     t.string   "cycle"
@@ -68,8 +68,12 @@ ActiveRecord::Schema.define(:version => 20120316154543) do
     t.datetime "created_at",                                :null => false
     t.datetime "updated_at",                                :null => false
     t.boolean  "admin",                  :default => false
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
