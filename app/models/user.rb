7a34f7d3c665585e01abf634e3307a44f93f3f0d
Reiner Marquez <rmaceissoft@gmail.com>
2012-03-19 18:01:42 -0500

    added first and last name fields on User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :admin, :as => :admin
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :admin, :first_name, :last_name, :as => :admin
+
+  def full_name
+    [first_name, last_name].join(' ')
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120316164607) do
+ActiveRecord::Schema.define(:version => 20120319224318) do
 
   create_table "invoice_cycles", :force => true do |t|
     t.string   "cycle"
@@ -71,6 +71,8 @@ ActiveRecord::Schema.define(:version => 20120316164607) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
