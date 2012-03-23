d397091fce38f12133bedf8174628a94dce44e1c
Reiner Marquez <rmaceissoft@gmail.com>
2012-03-22 20:02:56 -0500

    defined fixed options for status column. set unreviewed by default

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,7 @@ class Task < ActiveRecord::Base
   # class constants
   # get an array with 0.0, 0.25, 0.50, 0.75, ...
   @@hours_options = 0.step(3, 0.25).collect {|x| x}
+  @@statuses_options = { "u" => "Unreviewed", "uc" => "Unreviewed changed", "nc" => "Need Correction", "r" => "Reviewed"}
 
   # active record associations
   belongs_to :proyect
@@ -13,9 +14,21 @@ class Task < ActiveRecord::Base
   validates :hours, :inclusion => { :in => @@hours_options }
   validates :proyect, :presence => true
 
+  # callbacks
+  after_initialize do |obj|
+    if obj.new_record?
+      # initialize all task with unreviewed state
+      obj.status = "u"
+    end
+  end
+
   # helper methods
   # helper to get hours options
   def self.get_hours_options
     @@hours_options.map{ |z| [z,z]}
+  end
+
+  def self.get_statuses_options
+    @@statuses_options.map  { |k, v| [k, v] }
   end
 end
